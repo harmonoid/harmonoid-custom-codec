3608ae2061ad36f02be1233cf3a543ad5489af6f
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-02-09 18:36:18 +0530

    Update appveyor-build.sh

--- appveyor-build.sh
+++ appveyor-build.sh
@@ -2,7 +2,7 @@ export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
 
 sudo -E apt-get -qy update
-sudo -E apt-get -m -y install build-essential checkinstall bison flex gettext git mercurial subversion ninja-build gyp cmake yasm nasm automake pkg-config libtool libtool-bin gcc-multilib g++-multilib libgmp-dev libmpfr-dev libmpc-dev libgcrypt-dev gperf ragel texinfo autopoint re2c asciidoc docbook2x unzip p7zip-full curl  python3-pip libjpeg-dev zlib1g-dev tree
+sudo -E apt-get -m -y install build-essential checkinstall bison flex gettext git mercurial subversion ninja-build gyp cmake yasm nasm automake pkg-config libtool libtool-bin gcc-multilib g++-multilib libgmp-dev libmpfr-dev libmpc-dev libgcrypt-dev gperf ragel texinfo autopoint re2c asciidoc docbook2x unzip p7zip-full curl  python3-pip libjpeg-dev zlib1g-dev
 sudo pip3 install rst2pdf meson mako
 
 git config --global core.fileMode false
@@ -17,8 +17,6 @@ ninja gcc
 ninja mpv
 
 cat /home/runner/work/MPV-Windows-LGPL/MPV-Windows-LGPL/build64/packages/mpv-prefix/src/mpv-stamp/mpv-configure-*.log
-
-tree
 
 7z a mpv-dev.7z ./mpv-dev-x86_64*/*
 appveyor PushArtifact mpv-dev.7z
